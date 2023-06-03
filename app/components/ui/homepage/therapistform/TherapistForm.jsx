a20a02c1ba99e463c47c7e2802a0aa8d78bcfcd1
ZacharyCallahan <zachcallahan90@gmail.com>
2023-06-03 17:44:15 -0400

    Moved questions array

--- app/components/ui/homepage/therapistform/TherapistForm.jsx
+++ app/components/ui/homepage/therapistform/TherapistForm.jsx
@@ -1,14 +1,30 @@
-
 import TherapistHeader from "./TherapistHeader";
 import ThreapistHandler from "./TherapistHandler";
 
 const TherapistForm = () => {
-   
+    const questions = [
+        //array of object
+        {
+            question: "This is the question?", //object with question and answers
+            answerOne: "tests",
+            answerTwo: "answer asdasd",
+            answerThree: "answer asdasd",
+            answerFour: "answer asdasd",
+        },
+        {
+            //array of object
+            question: "This is the second question", //object with question and answers
+            answerOne: "1",
+            answerTwo: "2",
+            answerThree: "3",
+            answerFour: "4",
+        },
+    ];
 
     return (
         <div className="w-1/2 border-black border-2 p-6">
             <TherapistHeader />
-            <ThreapistHandler />
+            <ThreapistHandler questions={questions} />
         </div>
     );
 };
--- app/components/ui/homepage/therapistform/TherapistHandler.jsx
+++ app/components/ui/homepage/therapistform/TherapistHandler.jsx
@@ -2,35 +2,19 @@
 import { useState } from "react";
 import ThreapistQuestion from "./ThreapistQuestion";
 
-const TherapistHandler = () => {  
-    const questions = [
-        //array of object
-        {
-            question: "This is the question?", //object with question and answers
-            answerOne: "tests",
-            answerTwo: "answer asdasd",
-            answerThree: "answer asdasd",
-            answerFour: "answer asdasd",
-        },
-        {
-            //array of object
-            question: "This is the second question", //object with question and answers
-            answerOne: "1",
-            answerTwo: "2",
-            answerThree: "3",
-            answerFour: "4",
-        },
-    ];
+const TherapistHandler = ({ questions }) => {
+    // state value to keep track of the index of the current question
     const [currentQuestion, setCurrentQuestion] = useState(0);
     return (
         <div>
+
             {questions
                 //filters through questions array and returns the question at the index of currentQuestion (state value)
                 .filter((question, index) => index === currentQuestion)
-                // map through the question at the index of currentQuestion and return the question and answers 
+                // map through the question at the index of currentQuestion and return the question and answers
                 .map((question, index) => (
                     // pass the question and answers as props to the ThreapistQuestion component to render the string values from the questions array
-                    // also pass the setCurrentQuestion function as a prop to the ThreapistQuestion component to update the state value 
+                    // also pass the setCurrentQuestion function as a prop to the ThreapistQuestion component to update the state value
                     // of currentQuestion when a user clicks the next or prev button
                     <ThreapistQuestion
                         key={index}
