5b493881c3991adc8a5f0a60eaef0590dc27e33d
Randel Humphrey <randelhumphrey@gmail.com>
2023-10-04 12:25:01 -0400

    added submit button

--- app/components/ui/homepage/therapistform/ThreapistQuestion.jsx
+++ app/components/ui/homepage/therapistform/ThreapistQuestion.jsx
@@ -27,8 +27,15 @@ const ThreapistQuestion = ({
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(answers);
-    };
 
+        if (!user) {
+            console.log("User is not logged in. Please log in to continue.")
+            return;
+        }
+        //axios call to the backend to send the answers to the database
+        
+    };
+    
     return (
         <div className="space-y-6 ">
             {/* // renders the question and answers from the questions array via the values passed down from props (came from the TherapistHandler)  */}
@@ -71,11 +78,17 @@ const ThreapistQuestion = ({
                     setCurrentQuestion={setCurrentQuestion}
                     questionsLength={questionsLength}
                 />
-                <Next
-                    currentQuestion={currentQuestion}
-                    setCurrentQuestion={setCurrentQuestion}
-                    questionsLength={questionsLength}
-                />
+                {questionsLength === currentQuestion + 1 ? (
+                    <button
+                        className="bg-accent hover:bg-green-600 text-white py-2 px-4 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+                        onClick={handleSubmit}>
+                        Submit
+                    </button>
+                ) : <Next
+                currentQuestion={currentQuestion}
+                setCurrentQuestion={setCurrentQuestion}
+                questionsLength={questionsLength}
+            />}  
             </div>
         </div>
     );
